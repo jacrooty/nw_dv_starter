6a7e2ecc133516c9dc688403d1ee4023cf3c5e71
jacrooty <james.startup@gmail.com>
2018-04-23 21:16:48 -0700

    added reloading and another d3 example with require

--- main.js
+++ main.js
@@ -4,6 +4,9 @@ const app = electron.app
 // Module to create native browser window.
 const BrowserWindow = electron.BrowserWindow
 
+// Use electron-reload to improved development process
+require('electron-reload')(__dirname)
+
 const path = require('path')
 const url = require('url')
 
@@ -11,11 +14,32 @@ const url = require('url')
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
 
+console.log('executing main.js')
+
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 1000, height: 600})
+  console.log('creating mainWindow from main.js')
+  mainWindow = new BrowserWindow({
+    show: true,                  // default value = true
+    backgroundColor: "#FFF",     // default value = "#FFF"
+    width: 800,                  // default value = 800
+    height: 600,                 // default value = 600
+    minWidth: 0,                 // default value = 0
+    maxWidth: 1600,              // default value = unlimited
+    minHeight: 0,                // default value = 0
+    maxHeight: 1600,              // default value = unlimited
+    resizable: true,             // default value = true
+    movable: true,               // default value = true
+    alwaysOnTop: false,          // default value = false
+    title: "Electron",           // default value = "Electron"
+    frame: true,                 // default value = true
+    titleBarStyle: "default",    // default value = "default" (alternative "hidden")
+    transparent: false          // default value = false
+    // when setting transparent to true, comment out backgroundColor
+    })
 
   // and load the index.html of the app.
+  console.log('loading index.html into main browser window')
   mainWindow.loadURL(url.format({
     pathname: path.join(__dirname, 'index.html'),
     protocol: 'file:',
@@ -43,6 +67,7 @@ app.on('ready', createWindow)
 app.on('window-all-closed', function () {
   // On OS X it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
+  console.log("closing browser windows from main.js")
   if (process.platform !== 'darwin') {
     app.quit()
   }
